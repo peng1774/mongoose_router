f1adef00b1e2516cb17479543163e4d81808c511
zhulin peng <zhulin.peng@hermes-microvision.com>
2018-01-10 15:53:20 -0800

    unworking draft 1

---
+++ models/promotions.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+var promotionSchema = new Schema({
+	name: {
+		type: String,
+		require: true
+	},
+	image: {
+		type: String,
+		require: true
+	},
+	label: {
+		type: String,
+		require: true
+	},
+	price: {
+		type: Currency,
+		min: 0,
+		require: true
+	},
+	description: {
+		type: String,
+		require: true
+	}
+}
+,{timestamps: true}
+);
+
+var Promotions = mongoose.model('promotion',promotionSchema);
+module.exports = Promotions;
---
+++ routes/promoRouter.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const Promotions = require('../models/promotions');
+
+const promoRouter = express.Router();
+
+promoRouter.route('/')
+.get((req,res,next)=>{
+	Promotions.find({})
+	.then((promotions)=>{
+		res.statusCode = 200;
+		res.setHeader = ("Content-Type","application/json");
+		res.json(promotions);
+	},(err)=>next(err))
+	.catch((err)=>next(err));
+})
+.post((req,res,next)=>{
+	Promotions.create(req.body)
+	.then((dish)=>{
+		console.log('promotion created successfully');
+		res.statusCode = 200;
+		res.setHeader("Content-Type","application'json");
+		res.json(Promotions);
+	},(err)=>next(err))
+	.catch((err)=>next(err));
+})
+.put((req,res,next)=>{
+	res.statusCode = 403;
+	res.end('PUT operation not supported.');
+})
+.delete((req,res,next)=>{
+	Promotions.remove({})
+	.then((resp)=>{
+		res.statusCode = 200;
+		res.setHeader = ("Content-Type","application/json");
+		res.json(resp);
+	},(err)=>next(err))
+	.catch((err)=>next(err));
+});
+
+module.exports = promoRouter;
